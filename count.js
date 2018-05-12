7a6ae3daaf6d770523efcb79feaf6d37ec826958
Glitch (learnmongodb) <none>
2018-05-12 16:59:55 +0000

    🏬🙉 Checkpoint ./find_project.js:1080052/554 ./insert.js:1080052/687 ./update.js:1080052/648 ./remove.js:1080052/468 ./count.js:1080052/1058 ./aggregate.js:1080052/4 ./aggregate:1080052/340

--- count.js
+++ count.js
@@ -1,13 +1,18 @@
-var age = parseInt(process.argv[2]
+var age = parseInt(process.argv[2])
 var url = "mongodb://localhost:27017/learnyoumongo"
 var mongo = require("mongodb").MongoClient
 
 mongo.connect(url, function(err, db){
   if (err) throw err
   var parrots = db.collection("parrots")
-  parrots.count().toArray(function(err, data){
+  parrots.count({
+    age: {
+      $gt: age
+    }
+  },function(err, data){
     if (err) throw err
-    colsole.log(data)
+    console.log(data)
     db.close()
+  })
 })
   
