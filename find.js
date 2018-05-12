a56bcb7832469d6f528dd87587545f6887fee522
Glitch (learnmongodb) <none>
2018-05-12 13:50:39 +0000

    👋🐠 Checkpoint ./find.js:1080052/192

---
+++ find.js
@@ -0,0 +1,6 @@
+var url = "mongodb://localhost:27017/learnyoumongo";
+var mongo = require("mongodb").MongoClient;
+    
+mongo.connect(url, function(err, db) {
+      // db gives access to the database
+    });
