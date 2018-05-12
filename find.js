ddefdd756c73b2a909d17d30a54e51f3869be99b
Glitch (learnmongodb) <none>
2018-05-12 14:11:21 +0000

    🎸🎉 Checkpoint ./find.js:1080052/56

--- find.js
+++ find.js
@@ -1,15 +1,14 @@
 var url = "mongodb://localhost:27017/learnyoumongo";
-var age_passed = process.argv[2];
+var age_passed = parseInt(process.argv[2]);
 var mongo = require("mongodb").MongoClient;
  
-
 mongo.connect(url, function(conn_err, db) {
   if (conn_err) throw conn_err;
   var parrots = db.collection("parrots");
   parrots.find({
-    age: {$gt: +age_passed}
-  }).toArray(function(find_err, documents){
+    age: {$gt: age_passed}
+  }).toArray(function(find_err, data){
     if (find_err) throw find_err;
-    console.log(documents);
+    console.log(data);
   })
 });
