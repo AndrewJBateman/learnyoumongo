9f22aaf306792b3a66cab782280254c9da0ebd6d
Glitch (learnmongodb) <none>
2018-05-12 16:23:27 +0000

    🏅🙉 Checkpoint ./find_project.js:1080052/554 ./insert.js:1080052/687 ./update.js:1080052/476

--- update.js
+++ update.js
@@ -1,11 +1,18 @@
-var dbname = process.argv[2];
-var url = "mongodb://localhost:27017/"+dbname;
+var dbname = process.argv[2]
+var url = "mongodb://localhost:27017/"+dbname
 var mongo = require("mongo").MongoClient;
 
 mongo.connect(url, function(err, db){
   if (err) throw err
   var users = db.collection("users");
-  users.
-  
-  
+  users.update({
+    "username": "tinatime"
+  },{
+    $set:{
+      age: 40
+    }
+  }, function(err, data){
+    if (err) throw err
+    db.close();
+  })  
 })
