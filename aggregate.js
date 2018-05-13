462e128fd188420de7b087415d25ae71cc432258
Glitch (learnmongodb) <none>
2018-05-13 07:04:57 +0000

    🎪🚗 Checkpoint ./aggregate.js:1080052/56

--- aggregate.js
+++ aggregate.js
@@ -1,20 +1,12 @@
-/*
-output the average price
-of the items that match
-the size criteria input
-the average is converted
-to number and rounded to
-2 decimals
-*/
+/*Challenge: output the average price
+of the items that match the size criteria input
+the average is converted to number and rounded to
+2 decimals*/
 
 var mongoClient = require('mongodb').MongoClient;
-
 var collectionName = 'prices';
-
 var databaseName = 'learnyoumongo';
-
 var size = process.argv[2];
-
 var url = 'mongodb://localhost:27017/learnyoumongo';
 
 mongoClient.connect(url, function(err, DB) {
@@ -22,11 +14,11 @@ mongoClient.connect(url, function(err, DB) {
 
 	dataBase.collection(collectionName)
 			.aggregate([
-				{ $match: { size: size } },
-				{ $group: {
-					_id: 'average',
+				{ $match: { size: size } 
+        },
+				{ $group: { _id: 'average',
 					total: { $avg: '$price' } 
-				} }
+				}}
 			]).toArray(function(err, results) {
 				if (err) {
 					console.log(err);
